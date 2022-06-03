389504672afacf3df87f7f0d3f38263f9090695e
averichev <averichev@yandex.ru>
2022-06-03 13:06:26 +0300

    style: Code style

--- M13.InterviewProject/Program.cs
+++ M13.InterviewProject/Program.cs
@@ -1,15 +1,11 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace M13.InterviewProject
 {
     public static class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
