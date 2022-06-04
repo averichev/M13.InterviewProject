1d41ddbf1919203c82e62f56f029ac4689e7e42c
averichev <averichev@yandex.ru>
2022-06-04 15:25:17 +0300

    refactor: SpellChecker service

--- M13.InterviewProject/Services/Implementation/SpellChecker.cs
+++ M13.InterviewProject/Services/Implementation/SpellChecker.cs
@@ -1,8 +1,10 @@
 namespace M13.InterviewProject.Services.Implementation
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Net;
-    using System.Net.Http;
+    using Services;
     using System.Threading.Tasks;
     using Models;
     using Models.Implementation;
@@ -10,29 +12,25 @@ namespace M13.InterviewProject.Services.Implementation
 
     public class SpellChecker : ISpellChecker
     {
-        private const string YandexSpellCheckerUrl = "http://speller.yandex.net/services/spellservice.json/checkText?text=";
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SpellChecker(IHttpClientFactory httpClientFactory) => _httpClientFactory = httpClientFactory;
+
+        private const string YandexSpellCheckerUrl =
+            "https://speller.yandex.net/services/spellservice.json/checkText?text=";
+
         public async Task<ISpellCheckError[]> GetErrors(string text)
         {
             //используем сервис яндекса для поиска орфографических ошибок в тексте
             //сервис возвращает список слов, в которых допущены ошибки
-            Task<HttpResponseMessage> task = new HttpClient()
+            var response = await _httpClientFactory.CreateClient()
                 .GetAsync(YandexSpellCheckerUrl + WebUtility.UrlEncode(text));
 
-            var errors = task.ContinueWith(t =>
-            {
-                int count = 0;
-                var json = t.Result.Content.ReadAsStringAsync().Result;
-                var errs = JsonConvert.DeserializeObject<SpellerErrors[]>(json);
-                List<ISpellCheckError> list = new List<ISpellCheckError>(100);
-                for (int i = 0; i < errs.Length; i++)
-                {
-                    list.Add(errs[i]);
-                }
-                return list;
-            }).Result;
-
-            return errors.ToArray();
+            var json = await response.Content.ReadAsStringAsync();
+            var errors = JsonConvert.DeserializeObject<SpellerErrors[]>(json);
+            var list = new List<ISpellCheckError>(100);
+            list.AddRange((errors ?? Array.Empty<SpellerErrors>()).Cast<ISpellCheckError>());
+            return list.ToArray();
         }
     }
-
 }
