b1641d5e0063b8b2848269dba324db92c040c46d
averichev <averichev@yandex.ru>
2022-06-05 18:39:57 +0300

    style: Code style

--- M13.InterviewProject/Controllers/Api/RulesController.cs
+++ M13.InterviewProject/Controllers/Api/RulesController.cs
@@ -1,7 +1,6 @@
 namespace M13.InterviewProject.Controllers.Api
 {
     using System;
-    using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Logging;
